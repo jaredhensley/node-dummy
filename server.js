8baf18615705fde899ffd594a9e4dcbfab9d5b77
Jared Hensley <jahensle85@me.com>
2015-11-20 12:27:39 -0600

    express done

--- server.js
+++ server.js
@@ -1,25 +1,67 @@
-var http = require('http');
+var express = require("express");
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var stuff = require('node-day-one');
+var app = express();
 var resources = stuff.resources;
-console.log(resources);
 
-var server = http.createServer();
+var guestList = {
 
-var handleRequest = function (req, res) {
 
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Origin', 'OPTIONS, GET');
-  res.setHeader('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept');
+};
 
-  if (req.method === 'GET') {
-    res.statusCode = 200;
-    res.end(JSON.stringify(resources));
+//middleware
+app.use(function (req, res, next) {
+  console.log(`GETTING A ${req.method} REQUEST AT ${req.url}`);
+  next();
+});
+
+app.post('*', function (req, res, next) {
+  if (req.headers['content-type'] !== 'application/json') {
+    res.status(400).json('Expected content-type application/json');
+  } else {
+    next();
   }
-}
+});
+app.use(bodyParser.json());
+app.use(cors());
 
-server
-  .on('request', handleRequest)
+app.get('/resources', function (req, res, next) {
+
+  console.log('hi');
+  next();
+
+}, function (req, res) {
+  console.log('getting at slash root');
+  res.json(resources);
+});
+
+
+app.get('/guest-list', function (req, res) {
+
+  res.json(guestList);
+});
+
+app.post('/guest-list', function (req, res) {
+  var guests = req.body;
+  for (var key in guests) {
+    guestList[key] = guests[key];
+  }
+  res.json(true);
+});
+
+app.put('/guest-list/:guestName', function (req, res) {
+  var guestName = req.params.guestName;
+  guestList[guestName] = req.body.attending;
+  res.json(true);
+
+});
+/*app.post('/resources', function (req, res) {
+  console.log(req.body);
+
+})*/
+
+app
   .listen(9001, function (e) {
     if (e) return console.error(e);
     console.log('now listening on ', 9001);
